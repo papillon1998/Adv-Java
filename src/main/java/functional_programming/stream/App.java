092d53bf4885f1efcb9e6980e2bcca758f6229a4
Korosh <korosh.rad@sainsburys.co.uk>
2023-07-29 01:54:49 +0100

    added new method for q3

--- src/main/java/functional_programming/stream/App.java
+++ src/main/java/functional_programming/stream/App.java
@@ -2,6 +2,7 @@ package functional_programming.stream;
 
 
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -9,10 +10,6 @@ import java.util.stream.Collectors;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        List<Employee> employeeList = new ArrayList<>();
-
-        EmployeeFactory employeeFactory = new EmployeeFactory();
-        employeeList = employeeFactory.getAllEmployee();
 
     }
 
@@ -23,26 +20,17 @@ public class App {
                     .sorted(Comparator.comparing(Project::getName).reversed())
                     .collect(Collectors.toCollection(LinkedHashSet::new));
 
-
-
     /*2. Print full name of any employee whose firstName starts with ‘A’.*/
     Consumer<List<Employee>> employeesNameStartingWithA = list ->
             list.stream()
                     .filter(employee -> employee.getFirstName().startsWith("A"))
                     .forEach(employeeA -> System.out.println(employeeA.getFirstName() +" "+ employeeA.getLastName()));
 
-/* 3. List of all employee who joined in year 2023;
-    + getEmployeeByYear(List<Employee> employees, String year): List<Employee>
-       - iterate over the employees; stream
-       - employees.getId().getYearJoined
-       - filter out the employees matching the year
-       - collect them in a List:: ArrayList
- */
+/* 3. List of all employee who joined in year mentioned */
 
-
-//    BiFunction<List<Employee>, String, List<Employee>> getEmployeesByYear =
-//            (employeesList, year) -> employeesList.stream()
-//                    .filter(emp -> emp.getId().substring(0,4).ma )
-
+    BiFunction<List<Employee>, String, List<Employee>> getEmployeesJoinedInYear =
+            (employeesList, year) -> employeesList.stream()
+                    .filter(emp -> emp.getId().substring(0,4).equals(year))
+                    .collect(Collectors.toList());
 
 }
