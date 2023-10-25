cde214b880d8eaf4638e0eab1a2760c65b5e2954
Korosh <korosh.rad@sainsburys.co.uk>
2023-10-25 23:45:04 +0100

    updates

---
+++ src/main/java/collections/map/HashMap.java
@@ -0,0 +1,10 @@
+package collections.map;
+
+import java.util.Map;
+
+public class HashMap {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map1 = new java.util.HashMap<>();
+    }
+}
--- src/main/java/functional_programming/stream/EmployeeFactory.java
+++ src/main/java/functional_programming/stream/EmployeeFactory.java
@@ -1,6 +1,5 @@
 package functional_programming.stream;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
